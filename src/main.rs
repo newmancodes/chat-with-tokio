09f9127e1e5cdfed23ef0ccb8d4fd755448b4803
Stephen Newman <steve@newman.digital>
2021-06-25 04:15:59 +0100

    Multiple clients connect and multiple messages.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket, _addr) = listener.accept().await.unwrap();
-    let mut buffer = [0u8; 1024];
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
-    socket.write_all(&buffer[..bytes_read]).await.unwrap();
+
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (reader, mut write) = socket.split();
+            let mut reader  = BufReader::new(reader);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                write.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
+    }
 }
